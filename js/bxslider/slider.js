5215824997a65eb54a9cb61f3b8ae36293023351
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-02-14 01:52:10 +0200

    Clients

---
+++ js/bxslider/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.clients-items').bxSlider({
+        minSlides:5,
+        maxSlides:5,
+        slideWidth: 170,
+        slideMargin: 100,
+        auto: true,
+        moveSlides: 1
+    });
+});
