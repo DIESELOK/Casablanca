2e22519b0265860786389e634e620c75a4d67aff
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-02-13 22:20:49 +0200

    Fix Parallax

--- js/parallax.js
+++ js/parallax.js
@@ -3,13 +3,13 @@ $(window).scroll(function() {
     var st = $(this).scrollTop() /10;
 
     $(".hero .container").css({
-        "transform" : "translate3d(0px, -" + st  + "%, .01px)",
-        "-webkit-transform" : "translate3d(0px, -" + st  + "%, .01px)"
+        "transform" : "translate3d(0px, " + st  + "%, .01px)",
+        "-webkit-transform" : "translate3d(0px, " + st  + "%, .01px)"
     });
 
     $("#pure h2").css({
-        "transform" : "translate3d(0px, -" + st  + "%, .01px)",
-        "-webkit-transform" : "translate3d(0px, -" + st  + "%, .01px)"
+        "transform" : "translate3d(0px, " + st  + "%, .01px)",
+        "-webkit-transform" : "translate3d(0px, " + st  + "%, .01px)"
     });
 
 });
